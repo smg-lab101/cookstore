b58e0748459900aa17fe7831d31b58dd5900f601
vivigl <vivian.gleissner@gmail.com>
2025-04-25 08:03:45 +0200

    (feat) added handlebars for the three html files

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const path = require("path");
+const { engine } = require("express-handlebars");
+
 const app = express();
 const port = 3002;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public_html')));
+// Handlebars setup
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public_html")));
+app.use(express.urlencoded({ extended: true })); // for POST forms
+
+// Sample recipe data
+const recipes = require("./data/recipes.json");
+
+// Serving as handlebars template
 app.get("/list", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public_html', 'list.html'));
+  const recipeList = recipes.map(r => ({ ...r, link: `/detail/${r.id}` }));
+  res.render("list", { recipes: recipeList });
 });
 
-// Optional: routes to other pages
-app.get("/detail", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public_html', 'detail.html'));
+app.get("/detail/:id", (req, res) => {
+  const recipe = recipes.find(r => r.id === req.params.id);
+  if (!recipe) return res.status(404).send("Rezept nicht gefunden");
+  res.render("detail", recipe);
 });
 
 app.get("/new", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public_html', 'new.html'));
+  res.render("new");
 });
-
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
+
